1eb87734beeb5d3537c588ff2df475ed284bb9d9
TempGaurab <gaurabusa@gmail.com>
2024-10-28 23:49:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,9 +60,9 @@ def display_user_profile(profile):
 
 # Get the user_id from the URL parameters
 user_id = st.query_params.get("user_id", [None])[0]
-user_id = "1000" + str(user_id)
-
+# Proceed only if user_id is not None
 if user_id:
+    user_id = "1000" + str(user_id)
     user_profiles = read_profiles_from_json()
     profile = user_profiles.get(user_id)
 
